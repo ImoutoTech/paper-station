28d852a19f13412940ae6c24ec11ba6f4f46b5b2
季悠然 <youranreus@qq.com>
2024-05-24 12:51:33 +0800

    feat(backend): 配置文件,日志,类型转换,权限守卫基础配置

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,10 +1,43 @@
-import { Module } from '@nestjs/common';
+import { Module, ValidationPipe } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ENV_LIST } from '@/utils/const';
+import { AuthGuard, BusinessException, LoggerModule } from '@reus-able/nestjs';
+import { APP_GUARD, APP_PIPE } from '@nestjs/core';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [...ENV_LIST],
+    }),
+    LoggerModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_PIPE,
+      useFactory() {
+        return new ValidationPipe({
+          transform: true,
+          transformOptions: {
+            enableImplicitConversion: true, // 开启隐式转换
+          },
+          exceptionFactory: (errors) => {
+            const errorProperties = errors.map((e) => e.property).join(',');
+            return new BusinessException(
+              `参数校验失败，请检查 ${errorProperties}`,
+            );
+          },
+        });
+      },
+    },
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+  ],
 })
 export class AppModule {}
