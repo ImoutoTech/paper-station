78b533ab244ea556ce91f09a5224a7bd4771b8e4
季悠然 <youranreus@qq.com>
2024-08-18 17:25:16 +0800

    feat(backend): 站点dto

--- packages/backend/src/dto/site/create-site.dto.ts
+++ packages/backend/src/dto/site/create-site.dto.ts
@@ -1,1 +1,15 @@
-export class CreateSiteDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateSiteDto {
+  @IsString()
+  @IsNotEmpty()
+  configs: string;
+
+  @IsString()
+  @IsNotEmpty()
+  domains: string;
+
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+}
